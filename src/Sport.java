6692b50a8471265820b0240e618c12f06629c075
Wagister <61418245+Wagister@users.noreply.github.com>
2024-04-16 14:10:07 +0200

    add sport class

---
+++ src/Sport.java
@@ -0,0 +1,11 @@
+public class Sport {
+    private String nom;
+
+    public Sport(String nom) {
+        this.nom = nom;
+    }
+
+    public String getNomSport() {
+        return this.nom;
+    }
+}
