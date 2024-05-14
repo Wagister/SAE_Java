c8e6ed68439acdf8ac2ac6e0a1a7bc72469a9025
Mouadzouadi <143809277+Mouadzouadi@users.noreply.github.com>
2024-05-14 14:28:32 +0200

    Update Pays.java

---
+++ src/Pays.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pays {
+    private String nom;
+    private List<Participant> listeParticipants; 
+
+    public Pays(String nom) {
+        this.nom = nom;
+        this.listeParticipants = new ArrayList<>();
+    } 
+
+    public boolean ajouterParticipant(Participant participant) {
+        return this.listeParticipants.add(participant);
+    }   
+
+    public int getIndiceParticipant(Participant participant) {
+        return this.listeParticipants.indexOf(participant);
+    }
+
+    public boolean enleverParticipant(int indice) {
+        return this.listeParticipants.remove(indice);
+    }
+}
